5724db7678d647d89419de64a1764e84446a40a7
Willington Vega <wvega@wvega.com>
2019-02-22 09:37:24 -0500

    Configure PATH for Go and MySQL 5.7 formula.

--- .oh-my-custom-zsh/path.zsh
+++ .oh-my-custom-zsh/path.zsh
@@ -4,8 +4,16 @@ export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 # Python
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
+# Go development
+export GOPATH="${HOME}/.go"
+export GOROOT="/usr/local/opt/go/libexec"
+export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
+
+# Add path to binaries from mysql@5.7 formula
+export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
+
 # Custom binaries
-export PATH="$HONE/bin:$PATH"
+export PATH="$HOME/bin:$PATH"
 
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="$HOME/.rvm/bin:$PATH"
